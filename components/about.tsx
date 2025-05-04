7f1f7b139285f8da317677cd962224bd9fd2effe
SanjeevKandhaswamy <150614305+SanjeevKandhaswamy@users.noreply.github.com>
2025-05-04 14:52:08 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -36,7 +36,7 @@ export default function About() {
             </p>
             <div className="mt-8">
               <Button asChild>
-                <Link href="/Profile.pdf" target="_blank" download="Profile.pdf">
+                <Link href="/Resume.pdf" target="_blank" download="Profile.pdf">
                   Download Resume
                 </Link>
               </Button>
